dcd10557d6e00d14f8f7baffbdcf79a162930e08
hugoflc <hugo.folce.com@gmail.com>
2026-04-11 21:35:49 +0200

    Update bus_analysis.py

--- P26-Modélisation analyse technico/CODE PYTHON MODEL/bus_analysis.py
+++ P26-Modélisation analyse technico/CODE PYTHON MODEL/bus_analysis.py
@@ -136,13 +136,17 @@ def build_full_day_profile(
     )
  
     # Ajout d'un point à 0 kW après la fin de charge
-    last_row = charge_profile.iloc[[-1]].copy()
-    last_row["TimeHour"] = actual_charge_end_time + pd.Timedelta(minutes=1)
-    last_row["ChargePower_kW"] = 0.0
-    last_row["NetPower_kW"] = 0.0
-    last_row["EnergyCharged_kWh"] = 0.0
+    last_row1 = charge_profile.iloc[[-1]].copy()
+    last_row1["TimeHour"] = actual_charge_end_time + pd.Timedelta(minutes=1)
+    last_row1["ChargePower_kW"] = 0.0
+    last_row1["NetPower_kW"] = 0.0
+    last_row1["EnergyCharged_kWh"] = 0.0
+    last_row1["SoC"] = 100.0
+    
+    last_row2 = last_row1.copy()
+    last_row2["TimeHour"] = service_start_time + pd.Timedelta(days=1)
 
-    full_day_profile = pd.concat([service_profile, charge_profile.iloc[1:], last_row], ignore_index=True)
+    full_day_profile = pd.concat([service_profile, charge_profile.iloc[1:], last_row1, last_row2], ignore_index=True)   
     metadata = {
         "charge_power_kw": pcharge_kw,
         "charge_window_h": charge_window_h,
