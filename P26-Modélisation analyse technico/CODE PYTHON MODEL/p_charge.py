ac97941e81759051d950fe76d40be6cfaa6fdb4e
hugoflc <hugo.folce.com@gmail.com>
2026-04-12 15:09:13 +0200

    Update p_charge.py

--- P26-Modélisation analyse technico/CODE PYTHON MODEL/p_charge.py
+++ P26-Modélisation analyse technico/CODE PYTHON MODEL/p_charge.py
@@ -17,5 +17,6 @@ def p_charge(SoCi: float, Eb: float, TimeDifference: float, Pterminal: float) ->
     Pcharge : puissance de charge (kW)
     """
     Pcharge = min((1 - SoCi) * Eb / TimeDifference, Pterminal)
+    Pcharge = math.ceil(Pcharge + 1)
     Pcharge = 1.01 * Pcharge
     return Pcharge
